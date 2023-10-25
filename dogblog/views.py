407b79a1fbd43d78d276c477458ee1883495de47
shellym96 Maughan <shellymaughan10@gmail.com>
2023-10-25 11:23:07 +0000

    Update views with render requests

--- dogblog/views.py
+++ dogblog/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
-from django.http import HttpResponseRedirect
-from models.py import Dog, Competition, DogPhoto, LikePhoto 
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'dogstagram/home.html')
 
-class PostDetail(View):
-    """ get request """
-    def get(self, request, slug, **kwargs):
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
-        
-        return render(
-            request,
-            "like_details.html",
-            {
-                "post": post,
-                "liked": liked
-            },
-            )
+def photos(request):
+    return render(request, 'dogstagram/photos.html')
+
+def competition(request):
+    return render(request, 'dogstagram/competition.html')
+
+
+
+
