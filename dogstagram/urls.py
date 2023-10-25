8ac1571eb8a297e1687db366e85d50e42b3ca1ea
shellym96 Maughan <shellymaughan10@gmail.com>
2023-10-25 11:24:01 +0000

    Correct bugs in urls.py orginal file

--- dogstagram/urls.py
+++ dogstagram/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.PostDetail.as_view(), name='home'),
-    path('<slug:slug/>', views.PostDetail.as_view(), name='like_details'),
+    path('', include('dogblog.urls')),
 ]
