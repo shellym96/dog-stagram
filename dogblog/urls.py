2b3d6491e0909b69b2a141a45b61625d5c3e3ce8
shellym96 Maughan <shellymaughan10@gmail.com>
2023-10-31 14:29:24 +0000

    Add correct path for html links to work

--- dogblog/urls.py
+++ dogblog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from .views import home, photos, competition
 urlpatterns = [
-    path('', views.home),
-    path('photos', views.photos),
+    path('', views.home, name='home'),
+    path('photos/', views.photos, name='photos'),
+    path('competition/', views.competition, name='competition')
     
 ]
