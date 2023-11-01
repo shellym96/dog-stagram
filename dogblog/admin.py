3d3382024d0abafed32032a298616b9e2b818864
shellym96 Maughan <shellymaughan10@gmail.com>
2023-11-01 15:32:57 +0000

    Add functionality to admin.py file

--- dogblog/admin.py
+++ dogblog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Dog, Competition, DogPhoto, LikePhoto
 
-# Register your models here.
+
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'breed', 'dob')
+
+
+@admin.register(Competition)
+class CompetitionAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+
+@admin.register(DogPhoto)
+class DogPhotoAdmin(admin.ModelAdmin):
+    list_display = ('dog', 'competition')
+
+
+@admin.register(LikePhoto)
+class LikePhotoAdmin(admin.ModelAdmin):
+    list_display = ('person', 'dog_photo')
