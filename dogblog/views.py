1bb7eb8e5f9d7fa72f2dbcfa0cfed089ae7d78a2
shellym96 Maughan <shellymaughan10@gmail.com>
2023-11-01 15:34:17 +0000

    Add functionality to competition and photos

--- dogblog/views.py
+++ dogblog/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.http import HttpResponse
+from .models import Dog, Competition, DogPhoto, LikePhoto
 
 # Create your views here.
 def home(request):
-    return render(request, 'dogstagram/home.html')
+    return render(request, 'home.html')
 
 def photos(request):
-    return render(request, 'dogstagram/photos.html')
+    return render(request, 'photos.html')
 
 def competition(request):
-    return render(request, 'dogstagram/competition.html')
+    competitions = Competition.objects.all()
+    photos = DogPhoto.objects.all()
+    context = {
+        'competitions': competitions,
+        'photos': photos,
+    }
+    return render(request, 'competition.html', context)
 
 
 
