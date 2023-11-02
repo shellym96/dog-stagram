ab67c6e1c9c5da636eba8ddc6b3cc5534cafb149
shellym96 Maughan <shellymaughan10@gmail.com>
2023-11-02 15:11:03 +0000

    Add anew include path for allauth in urls

--- dogblog/urls.py
+++ dogblog/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import home, photos, competition
 urlpatterns = [
     path('', views.home, name='home'),
     path('photos/', views.photos, name='photos'),
-    path('competition/', views.competition, name='competition')
+    path('competition/', views.competition, name='competition'),
+    path('accounts/', include('allauth.urls')),
     
 ]
