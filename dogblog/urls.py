fb73830c3c4e61336eb37d362b4aba9daea67109
shellym96 Maughan <shellymaughan10@gmail.com>
2023-11-07 18:26:42 +0000

    Import add_photo, and create new path to add img

--- dogblog/urls.py
+++ dogblog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from . import views
-from .views import home, photos, competition
+from .views import home, photos, competition, add_photo
 urlpatterns = [
     path('', views.home, name='home'),
     path('photos/', views.photos, name='photos'),
     path('competition/', views.competition, name='competition'),
     path('accounts/', include('allauth.urls')),
     path('like/<int:id>', views.LikeDogPhoto.as_view(), name='photo_like'),
-    
+    path('add', add_photo, name='add')
 ]
