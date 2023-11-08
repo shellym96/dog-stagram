8a597a47215d66378a6155cb29fc26c27553c54f
shellym96 Maughan <shellymaughan10@gmail.com>
2023-11-08 12:24:22 +0000

    Delete cloudinary info and generate new keys

--- dogstagram/settings.py
+++ dogstagram/settings.py
@@ -158,9 +158,3 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'dze9qxopy',
-    'API_KEY': '371665636124297',
-    'API_SECRET': 'ew7LukK7Mrelf00zSP4E5L0Qva0'
-}
