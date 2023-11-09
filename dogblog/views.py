cdb7c23fa26df6c34fedab9dc6ccbe9a259a6781
shellym96 Maughan <shellymaughan10@gmail.com>
2023-11-09 22:16:20 +0000

    Add correct methods for competition

--- dogblog/views.py
+++ dogblog/views.py
@@ -13,17 +13,32 @@ def photos(request):
     return render(request, 'photos.html')
 
 def add_photo(request):
-    if request.method == "POST":
-        name = request.POST.get('photo_name')
-        Item.objects.create(name=name)
+    if request.method == 'POST':
+        dog_id = request.POST.get('dog')
+        competition_id = request.POST.get('competition')
+        form = DogPhotoForm(request.POST, request.FILES)
+        print(request.FILES)
+        dog = get_object_or_404(Dog, id=dog_id)
+        competition = get_object_or_404(Competition, id=competition_id)
 
-    return render(
-        request,
-        'templates/competition.html',
-        {
-            "comment_form": DogPhotoForm()
-        },
-    )
+        if form.is_valid():
+            dog_photo = form.save(commit=False)
+            dog_photo.dog = dog
+            dog_photo.competition = competition
+            dog_photo.save()
+
+            return HttpResponseRedirect(reverse('competition'))
+
+    else:
+        form = DogPhotoForm()
+
+    # Fetch dogs and competitions to pass to the template
+    dogs = Dog.objects.all()
+    print("dogs", dogs)
+    competitions = Competition.objects.all()
+    print("competitions", competitions)
+
+    return render(request, 'add_photo.html', {'form': form, 'dogs': dogs, 'competitions': competitions})
 
 
 def competition(request):
