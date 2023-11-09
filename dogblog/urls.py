8c8108bbeb26bc8968ff19be58f9b74b76bec136
shellym96 Maughan <shellymaughan10@gmail.com>
2023-11-09 22:13:53 +0000

    Add path for user to upload a photo to the site

--- dogblog/urls.py
+++ dogblog/urls.py
@@ -4,9 +4,8 @@ from .views import home, photos, competition, add_photo
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('photos/', views.photos, name='photos'),
     path('competition/', views.competition, name='competition'),
     path('accounts/', include('allauth.urls')),
     path('like/<int:id>', views.LikeDogPhoto.as_view(), name='photo_like'),
-    path('add', add_photo, name='add')
+    path('add_photo', add_photo, name='add_photo')
 ]
